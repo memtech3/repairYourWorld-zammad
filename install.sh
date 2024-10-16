367b1600ffda3a1ec84422e8582cc3089f70e4a5
Micah Guttman <memtech3@gmail.com>
2024-10-15 21:05:13 -0500

    use docker apt repo

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
+# configure docker apt repository
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
 sudo apt update
-sudo apt install -y docker docker-compose
-sudo systemctl stop sshd
-sudo systemctl disable sshd
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 sudo cp ./dockerComposeZammad.service /etc/systemd/system/dockerComposeZammad.service
 sudo systemctl enable dockerComposeZammad
